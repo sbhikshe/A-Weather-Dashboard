46578c0be54fd554e32021b531e94c24298a7b2c
Priya B <priya_saran@yahoo.com>
2022-10-09 15:51:01 -0700

    fix the button sizes, remove divs, cleanup console logs

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,7 @@
 formEl = $('#citySearchForm');
 searchInputEl = $('input[name="inputCity"]');
 searchBtnEl = document.querySelector("#searchCityBtn");
-searchHistoryUlEl =$('<ul>');
-citySearchHistoryDivEl = $('#citySearchHistory');
+citySearchHistoryUlEl = $('#citySearchHistory');
 
 /* inside col-10 container */
 var cityWeatherTodayEl = $('#cityWeatherToday'); // div
@@ -22,7 +21,7 @@ var currentDayObj = {
 
 /* 5 day forecast - DOM elements */
 /* Build this the first time, and maintain the references so
-we don't need to traverse the DOM everytime? */
+    we don't need to traverse the DOM everytime */
 var day0 = {
   dayLiEl: undefined,
   dateEl: undefined,
@@ -70,8 +69,6 @@ function handleCitySearchForm(event) {
   event.preventDefault();
 
   var inputCity = searchInputEl.val();
-  console.log("Input city: " + inputCity);
-
   if(inputCity != ""){
     if (addCityToSearchHistory(inputCity) == true) {
       displaySearchHistory();
@@ -86,7 +83,6 @@ function addCityToSearchHistory(inputCity) {
   if(!tmp) {
       searchHistory.push(inputCity);
       localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
-      addCityToSearchHistory(inputCity);
   } else {
     searchHistory = tmp;
     var found = false;
@@ -98,7 +94,6 @@ function addCityToSearchHistory(inputCity) {
     if(!found) {
       searchHistory.push(inputCity);
       localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
-      addCityToSearchHistory(inputCity);
     }
   }
   return true;
@@ -106,41 +101,33 @@ function addCityToSearchHistory(inputCity) {
 function displaySearchHistory() {
   var liEl;
   var cityBtnEl;
-  var searchHistoryUlEl;
-
-  console.log("in displaySearchHistory()");
 
   /* get search history from local storage */
   var tmp = JSON.parse(localStorage.getItem("searchHistory"));
   /* if there is one, then show the cities, else return right away */
   if (tmp) {
     /* not empty, iterate through it and show the cities */
-    console.log("Search history exists in local storage");
     searchHistory = tmp;
-    searchHistoryUlEl =$('<ul>');
+
+    /* if there are li attached to the ul, remove them? */
+    /* there won't be any li when the page refreshes, but there are 
+      items in the search history in local storage */
+    if(citySearchHistoryUlEl.children($('li'))){
+      citySearchHistoryUlEl.empty();
+    }
 
     for (var i = 0; i < searchHistory.length; i++){
       /* set up the button with the city name */
       /* add it to the li element */
-      console.log("City " + i + ": " + searchHistory[i]);
       liEl = $('<li>');
       cityBtnEl = $('<button>');
       cityBtnEl.text(searchHistory[i]);
-      cityBtnEl.addClass("btn btn-primary");
+      cityBtnEl.addClass("btn btn-primary btn-block mt-3");
       liEl.append(cityBtnEl);
   
-      //liEl.on('click', getForecast); - required? there is a listener on the ul?
       /* attach the list item to the list */
-      searchHistoryUlEl.append(liEl);
+      citySearchHistoryUlEl.append(liEl);
     }
-    /* if there is already an ul attached to the div, remove that
-    before adding this new ul */
-    /* attach the list to the city search history div */
-    if(citySearchHistoryDivEl.children($('ul'))){
-      console.log("Removing existing ul and attaching new one")
-      citySearchHistoryDivEl.empty();
-    }
-    citySearchHistoryDivEl.append(searchHistoryUlEl);
   } else {
     console.log("No search history");
   }
@@ -165,25 +152,19 @@ function getForecast(searchCity){
 }
 
 function getCurrentConditions(latitude, longitude) {
-  console.log("input City Coordinates : " + latitude, longitude);
 
   var requestUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=" + "a8f97ddc7ad9ecf69e905ace742d4325";
   fetch(requestUrl)
   .then (function(response){
-    console.log("Received response to get current weather conditions");
+    //console.log("Received response to get current weather conditions");
     response.json().then (function(data){
-      console.log("Current Conditions: ");
-      console.log(data);
-      // display the current conditions
+      //console.log(data);
       displayCurrentWeather(data);
     });
   });
 }
 
 function displayCurrentWeather(data) {
-  console.log("displayCurrentWeather");
-  var cityName = data.name;
-  var momentObj = moment.unix(data.dt);
  
   /* if the elements already exist, just need to set them */
   /* else create and add to the div */
@@ -209,48 +190,45 @@ function displayCurrentWeather(data) {
   }
 
   /* set the city name, date, icon, temp, wind, humidity values */
+  var momentObj = moment.unix(data.dt);
   currentDayObj.cityNameEl.text(data.name + " (" + momentObj.format("MMM D, YYYY") + ")");
-  var iconId = data.weather[0].icon;
-  currentDayObj.iconEl.attr("src", "http://openweathermap.org/img/w/" + iconId + ".png");
+  currentDayObj.iconEl.attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
   currentDayObj.cityNameEl.append(currentDayObj.iconEl); // CHECK !!! Why do this again?
 
+  /* temperature is in Kelvin, convert to Fahrenheit */
   var fahr = ((Number(data.main.temp) - 273.15) * 1.8 + 32).toFixed(0);
   currentDayObj.temperatureEl.text("Temperature: " + fahr + " " + '\u00b0' + "F");
   currentDayObj.windEl.text("Wind speed: " + data.wind.speed + " mph");
   currentDayObj.humidityEl.text("Humidity: " + data.main.humidity + "%");
 
+  /* make the current weather visible */
+  cityWeatherTodayEl.attr("style", "visibility: visible;");
+
 }
 
 function getFiveDayForecast(latitude, longitude) {
-  console.log("getFiveDayForecast");
-  console.log("input City Coordinates : " + latitude, longitude);
 
   //api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
   var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + "a8f97ddc7ad9ecf69e905ace742d4325";
   fetch(requestUrl)
   .then (function(response){
-    console.log("Received response to get 5 day forecast weather");
+    //console.log("Received response to get 5 day forecast weather");
     response.json().then (function(data){
-      console.log("Forecast Conditions: ");
-      console.log(data);
+      //console.log(data);
       displayFiveDayForecast(data);
     });
   });
 }
 
 function displayFiveDayForecast(data) {
-
-  console.log("displayFiveDayForecast: " + data);
  
   /* if the fiveDayForecastUlEl  is empty, create the 5 day DOM and add it to the ul */
   /* also maintain the references in the fiveDayForecastObj array */
   var daysLiEl = fiveDayForecastUlEl.children();
   if (daysLiEl.length === 0) {
-    console.log("set up the DOM for the day forecast");
     for ( var i = 0; i < 5; i++) {
-      console.log("setting up for day " + i + " forecast");
       fiveDayForecastObj[i].dayLiEl = $('<li>');
-      fiveDayForecastObj[i].dayLiEl.addClass("dayForecast");
+      fiveDayForecastObj[i].dayLiEl.addClass("dayForecast col-2 col-sm2- col-md-2 col-lg-2");
 
       fiveDayForecastObj[i].dateEl = $('<h5>');
       fiveDayForecastObj[i].dayLiEl.append(fiveDayForecastObj[i].dateEl);
@@ -269,48 +247,35 @@ function displayFiveDayForecast(data) {
       fiveDayForecastObj[i].dayLiEl.append(fiveDayForecastObj[i].humidityEl);
 
       fiveDayForecastUlEl.append(fiveDayForecastObj[i].dayLiEl);
-
-      console.log("added day obj to five day object array");
     }
   }
-  console.log("The list's children: " + fiveDayForecastUlEl.children().length );
-  console.log("The object array: " + fiveDayForecastObj);
 
+  /* For each day, set the date, icon, temp, wind, humidity values from the data */
+  var i = 0;
+  var prevDateStr = "";
 
-  /* set the date, icon, temp, wind, humidity values from the data */
-  /* for each of the days */
-  /* go through the data */
-      var i = 0;
-      var prevDateStr = "";
+  for (var j = 0; j < data.list.length; j++) {
+    var dateStrArr = data.list[j].dt_txt.split(" "); // dt_txt has date and time with " " between
+    var date = dateStrArr[0]; // get date only
 
-      for (var j = 0; j < data.list.length; j++) {
-        var dateStrArr = data.list[j].dt_txt.split(" "); // dt_txt has date and time with " " between
-        var date = dateStrArr[0]; // get date only
-        console.log("Filling " + i + "prevDateStr: " + prevDateStr + " list[" + j + "]:" + date);
+    if (prevDateStr !== date) {
+      /* this is an item in the list[] that has the next date */
+      /* so, grab the details from this item */
+      fiveDayForecastObj[i].dateEl.text(date);
+      fiveDayForecastObj[i].iconEl.attr("src", "http://openweathermap.org/img/w/" + data.list[j].weather[0].icon + ".png");
 
-        if (prevDateStr !== date) {
-          console.log("Got the next day");
-          /* this is an item in the list[] that has the next date */
-          /* so, grab the details from this item */
-          //day = fiveDayForecastObj[i];
-          console.log("Setting " + fiveDayForecastObj[i].dateEl.text());
-          fiveDayForecastObj[i].dateEl.text(date);
-          console.log("set to " + fiveDayForecastObj[i].dateEl.text());
-          fiveDayForecastObj[i].iconEl.attr("src", "http://openweathermap.org/img/w/" + data.list[j].weather[0].icon + ".png");
+      var fahr = ((Number(data.list[j].main.temp) - 273.15) * 1.8 + 32).toFixed(0);
+      fiveDayForecastObj[i].temperatureEl.text("Temp: " + fahr + " " + '\u00b0' + "F");
+      fiveDayForecastObj[i].windEl.text("Wind: " + data.list[j].wind.speed + " mph");
+      fiveDayForecastObj[i].humidityEl.text("Humidity: " + data.list[j].main.humidity + "%");
 
-          var fahr = ((Number(data.list[j].main.temp) - 273.15) * 1.8 + 32).toFixed(0);
-          fiveDayForecastObj[i].temperatureEl.text("Temp: " + fahr + " " + '\u00b0' + "F");
-          fiveDayForecastObj[i].windEl.text("Wind: " + data.list[j].wind.speed + " mph");
-          fiveDayForecastObj[i].humidityEl.text("Humidity: " + data.list[j].main.humidity + "%");
-
-          i++; 
-          prevDateStr = date;
-        } /* else skip this record, it is for the same day */
-        if (i == 5) {
-          console.log("Done getting forecast for all days");
-          break;
-        }
-      }
+      i++; 
+      prevDateStr = date;
+    } /* else skip this record, it is for the same day */
+    if (i == 5) {
+      break;
+    }
+  }
 }
 
 function handleCityFromHistory(event) {
@@ -328,4 +293,4 @@ function showDashboard() {
 
 $('document').ready(showDashboard);
 formEl.on('submit', handleCitySearchForm);
-citySearchHistoryDivEl.on('click', handleCityFromHistory);
+citySearchHistoryUlEl.on('click', handleCityFromHistory);
